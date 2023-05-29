8ee1b31a72edfe9bdee6ae5cfaa8f7c98f9e6d37
CrissCaban <82054860+CrissCaban@users.noreply.github.com>
2023-05-28 23:07:08 -0500

    upload project files

---
+++ guardar.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nombre = $_POST['nombre'];
+    $telefono = $_POST['telefono'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+
+    // Ruta del archivo de texto donde se guardarán los datos
+    $archivo = 'contactos.txt';
+
+    // Crear el contenido del registro
+    $registro = "Nombre: $nombre\nTelefono:$telefono\nEmail: $email\nMensaje: $mensaje\n\n";
+
+    // Guardar el registro en el archivo de texto
+    file_put_contents($archivo, $registro, FILE_APPEND | LOCK_EX);
+
+    echo '¡Gracias por contactarnos!';
+} else {
+    echo 'Ocurrió un error al procesar el formulario.';
+}
+?>
